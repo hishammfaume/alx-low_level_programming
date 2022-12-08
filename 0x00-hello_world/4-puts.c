4015d4094cfaa7769d89b1a007ba364f5e26af72
hishammfaume <hishammfaume@gmail.com>
2022-12-08 13:13:54 -0800

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/*
+ * main- priny a line of code using puts
+ * return: 0
+*/
+int main(void)
+{
+	puts("\n"Programming is like building a multilingual puzzle");
+		return (0);
+}
+
+ 
