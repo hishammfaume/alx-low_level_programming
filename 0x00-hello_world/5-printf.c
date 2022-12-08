1a357db13c868356904a86b7bb58cd0df84ba41b
hishammfaume <hishammfaume@gmail.com>
2022-12-08 13:37:18 -0800

    ptintf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -2,10 +2,10 @@
 /**
  * main - print a line of code using printf
  *
- * result: 0
+ * Return: 0
 */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-		return(0);
+		return (0);
 }
