22551a3b0f06997c1e0c6a21b5ab7ba40152afaf
hishammfaume <hishammfaume@gmail.com>
2022-12-11 02:57:02 -0800

    num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - prints numbers
+ *
+ * Return: 0
+*/
+int main(void)
+{
+	int numbers;
+
+	for (numbers = 0; numbers < 10; numbers++)
+	{
+		printf("%d\n", numbers);
+	}
+	return (0);
+}
