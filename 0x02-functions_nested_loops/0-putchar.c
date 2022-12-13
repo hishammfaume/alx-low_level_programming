2775588c15367fa9c69dc1d80028d6b27c6ba0ae
hishammfaume <hishammfaume@gmail.com>
2022-12-13 02:15:51 -0800

    putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Prints a code thats gives an output putchar
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	putchar('_');
+	putchar('p');
+	putchar('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+	return (0);
+}
