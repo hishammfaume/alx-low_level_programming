e1198d40d1f4b011e9127e21ab75e1c163461f60
hishammfaume <hishammfaume@gmail.com>
2022-12-14 09:36:12 -0800

    numbers

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * print_to_98 -  prints all natural number
+ * @n: character used
+ */
+void print_to_98(int n)
+{
+	if (n <= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+		printf("%d\n", n);
+	}
+}
