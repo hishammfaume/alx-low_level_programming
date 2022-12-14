8902bdc6014dd1290c5fa388b4065ec611d475ec
hishammfaume <hishammfaume@gmail.com>
2022-12-14 00:40:07 -0800

    add

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * add - adds two numbers
+ * @x: character tested
+ * @y: character tested
+ * Return: result of addition
+ */
+int add(int x, int y)
+{
+	return (x + y);
+}
