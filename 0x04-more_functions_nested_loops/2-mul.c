8a51fd1a1d1699e207b9aef6b4697600ec09874c
hishammfaume <hishammfaume@gmail.com>
2022-12-15 08:02:43 -0800

    mult

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * mul - multiplies two integers
+ * @a: character used
+ * @b: character used
+ * Return: multiplication
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
