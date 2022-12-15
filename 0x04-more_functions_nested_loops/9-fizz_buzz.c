fd8416fb0c2a0b2b46097cda485f2f439c541d47
hishammfaume <hishammfaume@gmail.com>
2022-12-15 12:01:22 -0800

    ipp

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,14 +1,14 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - prints numbers 
+ * main - prints numbers
  * Return: 0
  */
 int main(void)
 {
 	int i;
 
-	for (i = 1; i <=100; i++)
+	for (i = 1; i <= 100; i++)
 	{
 		if (i % 15 == 0)
 			printf("FizzBuzz");
