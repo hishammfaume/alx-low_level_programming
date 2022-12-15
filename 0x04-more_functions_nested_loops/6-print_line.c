beed3d09c5c7988b12c6905ce10931ea14e0bcd5
hishammfaume <hishammfaume@gmail.com>
2022-12-15 11:24:35 -0800

    ff

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_line - printing lines
+ * @n: number of times
+ * Return: void
+ */
+void print_line(int n)
+{
+	while (n-- > 0)
+	{
+		putchar('_');
+	}
+	putchar('\n');
+}
