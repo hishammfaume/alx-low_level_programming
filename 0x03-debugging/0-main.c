268c508cfb31ed1ed85056a24832cbf6bf2c763c
hishammfaume <hishammfaume@gmail.com>
2022-12-15 05:50:19 -0800

    test

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * main- tests function if integer is negative or positive
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 98;
+	positive_or_negative(i);
+
+	return (0);
+}
