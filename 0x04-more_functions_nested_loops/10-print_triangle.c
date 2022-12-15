43d14906eac7a8d72ce4442c41f5d082ec77bd16
hishammfaume <hishammfaume@gmail.com>
2022-12-15 12:11:52 -0800

    mmmg

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_triangle - entry point
+ * @size: size of the triangle
+ * Return: void
+ */
+void print_triangle(int size)
+{
+	int row, hashes, spaces;
+
+	if (size <= 0)
+	{
+		putchar('\n');
+	}
+	else
+	{
+		for (row = 1; row <= size; row++)
+		{
+			for (spaces = size - row; spaces >= 1; spaces--)
+			{
+				putchar(' ');
+			}
+			for (hashes = 1; hashes <= row; hashes++)
+			{
+				putchar('#');
+			}
+			putchar('\n');
+		}
+	}
+}
