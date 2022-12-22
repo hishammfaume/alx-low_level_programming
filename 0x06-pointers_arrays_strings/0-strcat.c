24beb1f6382256a472e2376f0d1352b338585484
hishammfaume <hishammfaume@gmail.com>
2022-12-22 00:40:38 -0800

    strcat

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _strcat - concatenates two strings
+ * @dest: character used
+ * @src: characterused
+ * Return: Returns a pointer to the resulting string
+ */
+char *_strcat(char *dest, char *src)
+{
+	int i, j;
+
+	for (i = 0; dest[i] != '\0'; i++)
+	for (j = 0; src[j] != '\0'; j++, i++)
+	{
+		dest[i] = src[j];
+	}
+	dest[i] = '\0';
+	return (dest);
+}
