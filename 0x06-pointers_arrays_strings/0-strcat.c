1fcca0ee61dcd21c6d99fec89f52dde26b24c2cd
hishammfaume <hishammfaume@gmail.com>
2022-12-22 01:01:28 -0800

    0-strcat.c

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -8,13 +8,14 @@
  */
 char *_strcat(char *dest, char *src)
 {
+	char *s = dest;
 	int i, j;
 
-	for (i = 0; dest[i] != '\0'; i++)
-	for (j = 0; src[j] != '\0'; j++, i++)
+	for (i = 0; *dest[i] != '\0'; i++)
+	for (j = 0; *src[j] != '\0'; j++, i++)
 	{
-		dest[i] = src[j];
+		*dest[i] = *src[j];
 	}
-	dest[i] = '\0';
-	return (dest);
+	*dest[i] = '\0';
+	return (s);
 }
