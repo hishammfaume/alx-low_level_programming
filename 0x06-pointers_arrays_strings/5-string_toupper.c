0eb96b28579dbbc7a1db692a702fc7a73ad86784
hishammfaume <hishammfaume@gmail.com>
2022-12-22 09:35:42 -0800

    upper

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -9,10 +9,12 @@ char *string_toupper(char *s)
 {
 	int i;
 
-	for (i = 0; s[i]; i++)
+	i = 0;
+	while (*(s + i))
 	{
-		if (s[i] >= 97 && s[i] <= 122)
-			s[i] -= 32;
+	if (*(s + i) >= 'a' && *(s + i) <= 'z')
+		*(s + i) -= 'a' - 'A';
+	i++;
 	}
-	return (0);
+	return (s);
 }
