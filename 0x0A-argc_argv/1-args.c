4a60b6e68e2075561dd8363ddf4b519aa3f3a0ac
hishammfaume <hishammfaume@gmail.com>
2023-01-12 09:21:13 +0000

    number

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints a number
+ * @argc: character used
+ * @argv: character used
+ * 
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	(void)argc;
+
+	printf("%d\n", argc);
+
+	return (0);
+}
