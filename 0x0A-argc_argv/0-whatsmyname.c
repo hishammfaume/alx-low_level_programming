bcce22cf16ffd1aa72bf4fbeef2b846dcbde5a5d
hishammfaume <hishammfaume@gmail.com>
2023-01-12 09:12:11 +0000

    myname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdlib.h>
+#include <stdio.h>
+/**
+ * main - prints a new name
+ * @argc: character used for counts
+ * @argv: character used 
+ * Return: 0
+ */
+int main(int argc, char *argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+	return (0);
+}
