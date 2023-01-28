bcba73dab3326ecc1ea05f53f7052e16782f74ca
hishammfaume <hishammfaume@gmail.com>
2023-01-28 11:57:36 +0000

    nn

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * *new_dog - prnts a new dog
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+
