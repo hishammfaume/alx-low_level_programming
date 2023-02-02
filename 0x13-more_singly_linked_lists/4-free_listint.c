ab6c69922857b3a0e7e42727062a79158220838b
hishammfaume <hishammfaume@gmail.com>
2023-02-02 07:16:16 +0000

    m

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+#include <stdlib.h>
+#include <stdio.h>
+
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+	temp = head;
+	head = head->next;
+	free(temp);
+	}
+}
+
